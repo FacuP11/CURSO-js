22b98afef728a00347078095d12ea9688c85e3b6
FacuP11 <facundo.ponceb@gmail.com>
2021-04-23 19:38:21 -0300

    clase1

---
+++ clase1/js/app.js
@@ -0,0 +1,17 @@
+
+
+
+let nombre = prompt ("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+
+let nacimiento = prompt("Ingrese su año de nacimiento");
+let resultado = 2021 - parseInt(nacimiento);
+let mensaje= (" Su nombre es " + nombre + " "+ apellido + " y su edad es " + resultado );
+
+
+alert (" Su nombre es " + nombre + " "+ apellido + " y su edad es " + resultado );
+
+console.log ("Mensaje de pruebilla");
+
+
+
